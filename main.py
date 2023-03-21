cf50092cfd194409b8177287b0baa9967ce25aa7
Jaqb-Janicqi <janicus321@gmail.com>
2023-03-21 22:33:40 +0100

    pep8

--- main.py
+++ main.py
@@ -5,22 +5,21 @@ import calendar
 import passanger_ride_generator as prg
 
 
-############## HOOBI tutorial section ##############           
+############## HOOBI tutorial section ##############
 # types = ["delay", "breakdown"]
 # x = np.random.randint(0, 2)
 # types[x]
 # delay = np.random.choice([0, 1], p=[0.9, 0.1])
-############## HOOBI tutorial section ##############     
+############## HOOBI tutorial section ##############
 
 
-
-####################################PROTOTYPING#################################################
+#################################### PROTOTYPING#################################################
 passenger_gen = prg(2018)
 passenger_gen.prg()
 print(passenger_gen.id)
 # pandas to csv
 # df = pd.DataFrame(passenger_gen.passanger_rides)
 # df.to_csv("passanger_rides.csv", index=False, header=False)
-####################################PROTOTYPING#################################################
+#################################### PROTOTYPING#################################################
 
-####################################DATA_GENERATION#################################################
+#################################### DATA_GENERATION#################################################
--- passanger_ride_generator.py
+++ passanger_ride_generator.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import calendar
 
+
 def working_day_distribution():
     morning_peak = np.random.normal(7.5, 1.6, 7000)
     all_day = np.random.uniform(0, 24, 1000)
@@ -13,6 +14,7 @@ def working_day_distribution():
     data = data[data >= 0]
     return data
 
+
 def friday_distribution():
     morning_peak = np.random.normal(7.5, 1.6, 7000)
     all_day = np.random.uniform(0, 24, 1000)
@@ -20,12 +22,14 @@ def friday_distribution():
     evening_peak = np.random.normal(16.5, 1.7, 7000)
     night_peak1 = np.random.normal(23.5, 1.3, 1000)
     night_peak2 = np.random.normal(0.5, 1.1, 700)
-    data = np.concatenate((morning_peak, all_day, evening_peak, mid_day, night_peak1, night_peak2))
+    data = np.concatenate(
+        (morning_peak, all_day, evening_peak, mid_day, night_peak1, night_peak2))
     data = data[data < 24]
     data = data[data >= 0]
     # plt.hist(data, bins=1440, alpha=0.5, label='gamma')
     # plt.show()
     return data
+
 
 def weekend_distribution():
     all_day = np.random.uniform(0, 24, 2000)
@@ -37,6 +41,7 @@ def weekend_distribution():
     # plt.show()
     return data
 
+
 class PassangerRide:
     def __init__(self, entry_section_id, exit_section_id, id, entry_time, exit_time):
         self.entry_section_id = entry_section_id
@@ -44,14 +49,15 @@ class PassangerRide:
         self.id = id
         self.entry_time = entry_time
         self.exit_time = exit_time
-    
+
     def __str__(self):
         # return all attributes in csv
         return f"{self.entry_section_id},{self.exit_section_id},{self.id},{self.entry_time},{self.exit_time}"
-    
+
 
 class PassangerGenerator:
-    sections = [1,3,2,1,3,1,2,2,1,3,1,2,3,1,3,1] # station0->1, station1->2
+    sections = [1, 3, 2, 1, 3, 1, 2, 2, 1, 3, 1,
+                2, 3, 1, 3, 1]  # station0->1, station1->2
 
     def __init__(self, year):
         self.id = 0
@@ -62,7 +68,8 @@ class PassangerGenerator:
         # generate calendar tuples for the year
         self.calendar_tuples = self.calendar_obj.yeardatescalendar(year, 12)
         # reshape calendar tuples to a list of dates
-        self.dates = [date for month in self.calendar_tuples for week in month for day in week for date in day]
+        self.dates = [
+            date for month in self.calendar_tuples for week in month for day in week for date in day]
 
     def generate_passanger_rides(self):
         for date in self.dates:
@@ -77,31 +84,39 @@ class PassangerGenerator:
             # plt.hist(dist, bins=1440, alpha=0.5, label='gamma')   # debug
             # plt.show()    # debug
             for minute_floor in range(1440):
-                minute_ceil = minute_floor + 1 # distribution is in hours, we are interested in 1 minute intervals
+                # distribution is in hours, we are interested in 1 minute intervals
+                minute_ceil = minute_floor + 1
                 condition = (dist >= minute_floor) & (dist < minute_ceil)
                 # num_passangers = np.count_nonzero(condition)
                 num_passangers = np.extract(condition, dist).size
                 hour = int(minute_floor/60)
-                minute = minute_floor%60
+                minute = minute_floor % 60
                 self.generate_ride(date, num_passangers, hour, minute)
-    
-    def generate_ride(self, date, num_passangers, hour, minute): # załadować excela, zobaczyć najbliższy pociąg i dopiero liczyć czas przejazdu
+
+    # załadować excela, zobaczyć najbliższy pociąg i dopiero liczyć czas przejazdu
+    def generate_ride(self, date, num_passangers, hour, minute):
         for i in range(num_passangers):
-            exit_section_id = entry_section_id = np.random.randint(1, self.num_sections + 1)
+            exit_section_id = entry_section_id = np.random.randint(
+                1, self.num_sections + 1)
             while exit_section_id == entry_section_id:
                 exit_section_id = np.random.randint(0, self.num_sections)
             if entry_section_id < exit_section_id:
-                travel_time = sum(self.sections[entry_section_id:exit_section_id])
+                travel_time = sum(
+                    self.sections[entry_section_id:exit_section_id])
             else:
-                travel_time = sum(self.sections[exit_section_id:entry_section_id])
-            entry_time = calendar.datetime.datetime(date.year, date.month, date.day, hour, minute, np.random.randint(0, 60))
-            exit_time = entry_time + calendar.datetime.timedelta(minutes=travel_time)
+                travel_time = sum(
+                    self.sections[exit_section_id:entry_section_id])
+            entry_time = calendar.datetime.datetime(
+                date.year, date.month, date.day, hour, minute, np.random.randint(0, 60))
+            exit_time = entry_time + \
+                calendar.datetime.timedelta(minutes=travel_time)
             exit_sec = entry_time.second
             while exit_sec == entry_time.second:
                 exit_sec = np.random.randint(0, 60)
             exit_time = exit_time.replace(second=exit_sec)
             # print(entry_time) # debug
             # print(exit_time) # debug
-            passenger_ride = PassangerRide(entry_section_id, exit_section_id, self.id, entry_time, exit_time)
+            passenger_ride = PassangerRide(
+                entry_section_id, exit_section_id, self.id, entry_time, exit_time)
             self.passanger_rides.append(passenger_ride)
             self.id += 1
