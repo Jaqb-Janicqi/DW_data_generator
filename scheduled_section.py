bdc6901f7528a284f4705b53beb8a5066d9750d7
Jaqb-Janicqi <janicus321@gmail.com>
2023-03-22 22:05:00 +0100

    scheduled section generator

---
+++ scheduled_section.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import calendar
+import datetime
+
+class ScheduledSection():
+    def __init__(self, section_id, start_station_id, end_station_id, arrival_time):
+        self.id = section_id
+        self.start_station_id = start_station_id
+        self.end_station_id = end_station_id
+        self.arrival_time = arrival_time
+
+    def __str__(self):
+        return f"{self.id},{self.start_station_id},{self.end_station_id},{self.arrival_time}"
+    
+class ScheduledSectionGenerator():
+    def __init__(self, year, timetable):
+        self.timetable = timetable
+        self.scheduled_sections = []
+        self.year = year
+        self.calendar_obj = calendar.Calendar()
+        # generate calendar tuples for the year
+        self.calendar_tuples = self.calendar_obj.yeardatescalendar(year, 12)
+        # reshape calendar tuples to a list of dates
+        self.dates = [
+            date for month in self.calendar_tuples for week in month for day in week for date in day]
+
+    def generate_scheduled_sections(self):
+        id = 0
+        for i in range(len(self.timetable)):
+            for j in range(len(self.timetable[i])):
+                for k in range(len(self.timetable[i][j])):
+                    # if self.timetable[i][j][k] is not of type datetime.time, convert it to datetime.time
+                    if not isinstance(self.timetable[i][j][k], datetime.time):
+                        self.timetable[i][j][k] = self.timetable[i][j][k].time()
+                    if i%2 == 0:
+                        self.scheduled_sections.append(ScheduledSection(id, k, k+1, self.timetable[i][j][k]))
+                    else:
+                        self.scheduled_sections.append(ScheduledSection(id, k+1, k, self.timetable[i][j][k]))
+                    id += 1
+
+    def to_csv(self):
+        with open('scheduled_sections.csv', 'w') as file:
+            file.write('id,start_station_id,end_station_id,arrival_time\n')
+            for section in self.scheduled_sections:
+                file.write(str(section) + '\n')
+
+timetable0 = pd.read_excel("timetable0.xlsx") # datetime.time
+timetable1 = pd.read_excel("timetable1.xlsx") # datetime.time
+timetable0 = timetable0.values.tolist()
+timetable1 = timetable1.values.tolist()
+timetables = [timetable0, timetable1]
+
+scheduled_section_generator = ScheduledSectionGenerator(2018, timetables)
+scheduled_section_generator.generate_scheduled_sections()
+scheduled_section_generator.to_csv()
