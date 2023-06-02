1808191c51b295e7438cee913a68fa005bcecbae
Jaqb-Janicqi <janicus321@gmail.com>
2023-06-02 20:14:18 +0200

    longer_simulation_time

--- main.py
+++ main.py
@@ -20,26 +20,31 @@ def snapshot(trains, scheduled_sections, rides, real_sections, passanger_rides):
         for train in trains:
             ids.append(train.Trainid)
         ids.sort()
+        print(ids[-1])
         for train in trains:
             file1.write(str(train) + '\n')
-        
+    file1.close()        
     with open(f'data/scheduled_sections{snapshots_made}.csv', 'w', encoding='UTF-8') as file2:
         for scheduled_section in scheduled_sections:
             file2.write(str(scheduled_section) + '\n')
+    file2.close()
     with open(f'data/real_sections{snapshots_made}.csv', 'w', encoding='UTF-8') as file3:
         for real_section in real_sections:
             file3.write(str(real_section) + '\n')
+    file3.close()
     with open(f'data/passanger_rides{snapshots_made}.csv', 'w', encoding='UTF-8') as file4:
         for passanger_ride in passanger_rides:
             file4.write(str(passanger_ride) + '\n')
+    file4.close()
     with open(f'data/rides{snapshots_made}.csv', 'w', encoding='UTF-8') as file5:
         for ride in rides:
             file5.write(str(ride) + '\n')
+    file5.close()
 
 
-simulation_start_time = datetime.datetime(2021, 1, 1, 0, 0, 0)
+simulation_start_time = datetime.datetime(2019, 1, 1, 0, 0, 0)
 current_time = simulation_start_time
-simulation_end_time = datetime.datetime(2021, 1, 5, 0, 0, 0)
+simulation_end_time = datetime.datetime(2021, 12, 30, 0, 0, 0)
 section_times = [1, 3, 2, 1, 3, 1, 2, 2, 1, 3,
                  1, 2, 3, 1, 3, 1]  # station0->1, station1->2
 
@@ -120,13 +125,18 @@ while current_time < simulation_end_time:
     minute += 1
     current_time += datetime.timedelta(minutes=1)
 
-for station in metro_line:
-    print(len(station))
+# ids = []
+# trains = train_generator.trains + train_generator.free_trains
+# for train in trains:
+#     ids.append(train.Trainid)
+# ids.sort()
+# for id in ids:
+#     print(id)
 
 snapshot(train_generator.trains, scheduled_sections,
          rides, real_sections, passanger_rides)
 print(f"Rides made: {ride_id}")  # debug
 
-print(len(passanger_rides))
+
 toc = time.perf_counter()
 print(f"Generated data in {toc - tic:0.4f} seconds")
